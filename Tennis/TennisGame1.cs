fe94660b11ba10e66aa349630a46996a8b8bd7b2
Olga Dolgova <olgd@equinor.com>
2023-04-12 15:33:23 +0200

    rename m_scores

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -2,8 +2,8 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
+        private int matchScorePlayer1 = 0;
+        private int matchScorePlayer2 = 0;
         private string player1Name;
         private string player2Name;
 
@@ -16,18 +16,18 @@ namespace Tennis
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                matchScorePlayer1 += 1;
             else
-                m_score2 += 1;
+                matchScorePlayer2 += 1;
         }
 
         public string GetScore()
         {
             string score = "";
             var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (matchScorePlayer1 == matchScorePlayer2)
             {
-                switch (m_score1)
+                switch (matchScorePlayer1)
                 {
                     case 0:
                         score = "Love-All";
@@ -44,9 +44,9 @@ namespace Tennis
 
                 }
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (matchScorePlayer1 >= 4 || matchScorePlayer2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
+                var minusResult = matchScorePlayer1 - matchScorePlayer2;
                 if (minusResult == 1) score = "Advantage player1";
                 else if (minusResult == -1) score = "Advantage player2";
                 else if (minusResult >= 2) score = "Win for player1";
@@ -56,8 +56,8 @@ namespace Tennis
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
+                    if (i == 1) tempScore = matchScorePlayer1;
+                    else { score += "-"; tempScore = matchScorePlayer2; }
                     switch (tempScore)
                     {
                         case 0:
