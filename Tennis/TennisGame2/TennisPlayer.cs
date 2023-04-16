b50de353e988d001017643c0589ee0dec3e7da1b
Eirik Nysted <eirik.nysted@bouvet.no>
2023-04-16 16:20:07 +0200

    added TennisPlayer class

---
+++ Tennis/TennisGame2/TennisPlayer.cs
@@ -0,0 +1,25 @@
+﻿namespace Tennis.TennisGame2
+{
+    internal class TennisPlayer
+    {
+        public string Name { get; }
+        public int Points { get; private set; }
+
+        public TennisPlayer(string name)
+        {
+            Name = name;
+            Points = 0;
+        }
+
+        public void AddPoint()
+        {
+            Points++;
+        }
+
+        public bool IsPlayerPointsDeuceOrMore()
+        {
+            var deucePoints = 4;
+            return Points >= deucePoints;
+        }
+    }
+}
